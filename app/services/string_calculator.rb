c16126f6c671e8e70862d756ee608ddaa5ae61a6
swathipasham <swathireddi.k@gmail.com>
2024-12-28 01:59:41 -0800

    Handled basic functionality for empty string, one number  and sum of 2 numbers
    Added test suites for mulitples numbers and new lines in the given input numbers

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -1,4 +1,7 @@
 class StringCalculator
   def self.add(numbers)
+    return 0 if numbers.empty?
+    return numbers.to_i if numbers.length == 1
+    return numbers.split(',')[0].to_i + numbers.split(',')[1].to_i
   end
 end
--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -11,14 +11,26 @@ RSpec.describe StringCalculator, type: :service do
     end
 
     context 'when the input is one number' do
-      it 'returns 1' do
+      it 'returns number' do
         expect(StringCalculator.add('1')).to eq(1)
       end
     end
 
     context 'when the input is two numbers' do
-      it 'returns  2' do
-        expect(StringCalculator.add('1,5')).to eq(2)
+      it 'returns sum of the numbers' do
+        expect(StringCalculator.add('1,5')).to eq(6)
+      end
+    end
+
+    context 'when the input is more than 2 numbers' do
+      it 'returns the sum of multiple numbers' do
+        expect(StringCalculator.add("1,2,3,4,5,1,2,3")).to eq(21)
+       end
+    end
+
+    context 'when the input has new line character' do
+      it 'returns the sum of the numbers' do
+        expect(StringCalculator.add("1\n2,3")).to eq(6)
       end
     end
   end
